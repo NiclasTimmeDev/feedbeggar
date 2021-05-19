2b75a2b8715e9aae41c7e1a6c02c8461c70e8ccf
Niclas <niclastimmdev@gmail.com>
2021-05-19 16:30:05 +0200

    remove redis from permission helper

--- main/backend/app/Helpers/PermissionHelper.php
+++ main/backend/app/Helpers/PermissionHelper.php
@@ -4,7 +4,6 @@ namespace App\Helpers;
 
 use App\Models\User;
 use App\Models\Project;
-use Illuminate\Support\Facades\Redis;
 
 class PermissionHelper
 {
@@ -56,26 +55,11 @@ class PermissionHelper
             return false;
         }
 
-        // Check if we have cached access.
-//        $access_cached = Redis::get('project_access_' . $user->id . '_' . $project_id);
-//
-//        // Positive access stored in cache.
-//        if((int)$access_cached === 1){
-//            return $user->projects()->where('id', $project_id)->get()->values()[0];
-//        }
-//
-//        // Access forbidden from cache.
-//        if((int) $access_cached === 0) {
-//            return false;
-//        }
 
         // Check if user has permission to access the process.
         $projects = $user->projects()->where('id', $project_id)->get()->values()->all();
 
         $project = $projects[0];
-
-        // Cache the result.
-        Redis::set('project_access_' . $user->id . '_' . $project_id, $project instanceOf Project ? 1 : 0);
 
         // Return the access.
         if (!$project instanceof Project) {
